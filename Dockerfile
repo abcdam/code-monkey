68f2b4979346a6fff6bf7f3241f5da1dc5682fbd
abcdam <lukasada@students.zhaw.ch>
2024-07-29 18:02:59 +0200

    Implement a good enough RAG, additional performance boost, and cleaned up setup code

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,26 @@
 FROM ghcr.io/open-webui/open-webui:ollama 
-# WORKDIR /app
-WORKDIR /app
 
-RUN apt update \
-    && apt install -y \
-    lshw 
+RUN apt update          \
+    && apt install -y   \
+    lshw                \
+    rsync
 
 COPY ./Modelfile /tmp/Modelfile
 
-RUN ollama serve & sleep 2 \
-    && ollama create code-monkey -f /tmp/Modelfile
+RUN ollama serve & sleep 2                          \
+    && ollama create code-monkey -f /tmp/Modelfile  \
+    && ollama pull mxbai-embed-large:latest         \
+    && cp -r /root/.ollama /tmp/.ollama
+
+COPY ./quickfix.sh .
+
+ENV WEBUI_AUTH=false                                
+ENV USE_CUDA_DOCKER=true                            
+ENV RAG_TOP_K=10                                    
+ENV RAG_EMBEDDING_ENGINE=ollama                     
+ENV RAG_EMBEDDING_MODEL=mxbai-embed-large:latest    
+ENV CHUNK_SIZE=1024                                 
+ENV CHUNK_OVERLAP=220
+
+CMD [ "bash", "quickfix.sh" ]
+
--- launch-code-monkey.sh
+++ launch-code-monkey.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-DOCSPATH=$(realpath $1)
-IMG_NAME=$2
-[ ! $DOCSPATH ] && echo "missing docs path argument" && exit 1
-[ ! $IMG_NAME ] && echo "missing image name" && exit 1
+script_dir=$(dirname "$(realpath "$0")")
+source "$script_dir/../sammelsurium/routines/helper.sh"
 
-git clone https://github.com/open-webui/open-webui.git
+CONTEXT_DIR=$(realpath "$1") || throw "First Argument: Context directory couldn't be parsed."
+[ -n "$2" ] && IMG_NAME=$2 || throw "Second Argument: No image name provided."
+[ -d $CONTEXT_DIR ] || throw "Directory '$CONTEXT_DIR' existence check failed."
+
+# given docker image name must fully match an available docker image
+docker image ls | awk 'NR > 1 {print $1}' | grep "^$IMG_NAME$" 1>/dev/null \
+    || throw "Docker image name '$IMG_NAME' couldn't be retrieved from local registry "
+
+if [ -d "$(dirname $0)/open-webui" ]; then cd open-webui && git pull && cd ..
+else git clone https://github.com/open-webui/open-webui.git || throw "cloning open-webui failed."
+fi
+
+mkdir -p ./data/docs
+EXPOSED_DIR='./data/docs'
+# make user given directory discoverable by open-webui 
+rsync -hvrP --exclude .git/ $CONTEXT_DIR $EXPOSED_DIR
 
 CONTAINER_NAME=code-monkey-webui
 mkdir -p ollama
-docker run --rm --detach --publish 3000:8080 --runtime=nvidia --gpus=all --env WEBUI_AUTH=False --volume ./ollama:/root/.ollama -v ./open-webui:/app/backend/data -v $DOCSPATH:/app/backend/data/docs --name $CONTAINER_NAME $IMG_NAME
+docker run --rm --detach --publish 3000:8080 --runtime=nvidia --gpus=all --env DATA_DIR=/tmp/data --volume ./ollama:/root/.ollama -v ./data:/tmp/data --name $CONTAINER_NAME $IMG_NAME
 
-# this is a workaround to update ollama to latest version to fix a bug when offloading layers to gpu in mistral model
-
-docker exec -it $CONTAINER_NAME kill -9 13
-docker exec -it $CONTAINER_NAME curl -fsSL https://ollama.com/install.sh | sh
-docker exec -it $CONTAINER_NAME ollama serve &
-docker container logs $CONTAINER_NAME
+docker container logs -f $CONTAINER_NAME
